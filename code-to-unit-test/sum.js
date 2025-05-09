82af3cae24a6b0a21d8876cf5e6ccbe4d20e26a9
Prachi Heda <prachiheda@gmail.com>
2025-05-09 15:18:53 -0700

    added jsdocs to test sum funciton

--- code-to-unit-test/sum.js
+++ code-to-unit-test/sum.js
@@ -1,3 +1,10 @@
+/**
+ * Returns the sum of two numbers.
+ *
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The result of adding `a` and `b`.
+ */
 export function sum(a, b) {
     return a + b;
   }
