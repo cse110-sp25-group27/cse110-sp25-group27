54cc133569111d24793091787f24f49fc468bc28
Prachi Heda <prachiheda@gmail.com>
2025-05-09 15:25:06 -0700

    change which files documentaiton reads

--- code-to-unit-test/sum.js
+++ code-to-unit-test/sum.js
@@ -1,10 +1,9 @@
 /**
- * Returns the sum of two numbers.
- *
- * @param {number} a - The first number.
- * @param {number} b - The second number.
- * @returns {number} The result of adding `a` and `b`.
+ * Adds two numbers.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
  */
 export function sum(a, b) {
-    return a + b;
+  return a + b;
 }
