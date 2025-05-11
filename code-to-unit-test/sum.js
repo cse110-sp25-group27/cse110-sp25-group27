843a873ddd749c3def46868f494d84a345032334
Prachi Heda <prachiheda@gmail.com>
2025-05-11 12:43:37 -0700

    added linting to yml, fake test to catch lint

--- code-to-unit-test/sum.js
+++ code-to-unit-test/sum.js
@@ -5,5 +5,11 @@
  * @returns {number}
  */
 export function sum(a, b) {
-  return a + b;
-}
+  let unusedVar = 10; // ❌ ESLint: 'unusedVar' is assigned a value but never used.
+
+  if (a == null) {     // ❌ ESLint: '==' should be '==='
+    console.log("Missing a"); // ✅ But side-effect may be flagged if console not allowed
+  }
+
+  return a + b
+} // ❌ ESLint: Missing semicolon
