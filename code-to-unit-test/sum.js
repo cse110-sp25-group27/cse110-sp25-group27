0bfc11090a106b2a629b236e353f6e911cf8a488
Prachi Heda <prachiheda@gmail.com>
2025-05-11 13:09:43 -0700

    fixed fake example

--- code-to-unit-test/sum.js
+++ code-to-unit-test/sum.js
@@ -5,11 +5,5 @@
  * @returns {number}
  */
 export function sum(a, b) {
-  let unusedVar = 10; // ❌ ESLint: 'unusedVar' is assigned a value but never used.
-
-  if (a == null) {     // ❌ ESLint: '==' should be '==='
-    console.log("Missing a"); // ✅ But side-effect may be flagged if console not allowed
-  }
-
-  return a + b
-} // ❌ ESLint: Missing semicolon
+  return a+b; 
+} 
