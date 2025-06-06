ad8cdce6e14330a8bbe06e491f2afee97ef19391
Srujam-Dave <srdave@uscd.edu>
2025-06-06 15:50:42 -0700

    synched e2e tests with branch

--- __tests__/e2e.test.js
+++ __tests__/e2e.test.js
@@ -27,9 +27,9 @@ describe('Basic user flow for onboarding', () => {
     await page.goto("https://cse110-sp25-group27.github.io/cse110-sp25-group27/frontend/pages/onboarding.html");
     await page.evaluate(() => localStorage.clear());
 
-    await page.click('[data-preset-id="p4"] .select-button', { delay: 1000 });
-    await page.click('[data-preset-id="p4"] .save-card-details-button', { delay: 1000 });
-    await page.click("#save-onboarding-button", { delay: 1000 });
+    await page.click('[data-preset-id="p4"] .select-button', { delay: 100 });
+    await page.click('[data-preset-id="p4"] .save-card-details-button', { delay: 100 });
+    await page.click("#save-onboarding-button", { delay: 100 });
 
     const onboarded = await page.evaluate(() => {
         return localStorage.getItem('hasCompletedOnboarding');
